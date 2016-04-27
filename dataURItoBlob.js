466e2b4d7e7f4d9c33fe041af91c4c3a47b2ee1f
Thomas Grainger <tom.grainger@procensus.com>
2016-04-27 15:37:03 +0100

    modern version
    http://www.nixtu.info/2013/06/how-to-upload-canvas-data-to-server.html

--- dataURItoBlob.js
+++ dataURItoBlob.js
@@ -1,18 +1,18 @@
 function dataURItoBlob(dataURI) {
     // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
     var byteString = atob(dataURI.split(',')[1]);
 
     // separate out the mime component
     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
 
     // write the bytes of the string to an ArrayBuffer
-    var arrayBuffer = new ArrayBuffer(byteString.length);
-    var _ia = new Uint8Array(arrayBuffer);
-    for (var i = 0; i < byteString.length; i++) {
-        _ia[i] = byteString.charCodeAt(i);
+    var ab = new ArrayBuffer(byteString.length);
+    var dw = new DataView(ab);
+    for(var i = 0; i < byteString.length; i++) {
+        dw.setUint8(i, byteString.charCodeAt(i));
     }
 
-    var dataView = new DataView(arrayBuffer);
-    var blob = new Blob([dataView], { type: mimeString });
-    return blob;
+    // write the ArrayBuffer to a blob, and you're done
+    return new Blob([ab], {type: mimeString});
 }
